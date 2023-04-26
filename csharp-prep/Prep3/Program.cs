db3db4e8bc48044aa89f62d13763e2fb22a33d95
herendira <here.baez@gmail.com>
2023-04-25 20:43:06 -0400

    Guess Number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess_number=-1;
+        //int counting=0;
+        string ans="yes";
+
+        while (ans=="yes")
+         {  
+            int counting=0;
+            while (guess_number != number)
+            {
+              Console.Write("What is your guess? ");
+              guess_number=int.Parse(Console.ReadLine());
+              counting=counting+1;
+              if (number > guess_number)
+              {
+                Console.WriteLine("Higher");
+              }
+
+              else if (guess_number< number)
+              {
+               Console.WriteLine("Lower");
+              }
+
+              else
+              {
+               Console.WriteLine("You guessed it!!");
+               Console.WriteLine($"You tried {counting} times");
+               counting=0;
+               }
+            }
+        Console.Write("Do you want to play again? yes or not ");
+        ans=Console.ReadLine();
+            
+        if (ans=="not")
+             {
+               Console.WriteLine("Thanks for playing with me ");
+             }
+        
+        
+        }
+    }    
 }
