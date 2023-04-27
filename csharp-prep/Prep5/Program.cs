eae21b8ca57ec167a5871816f2fdb26af19a7149
herendira <here.baez@gmail.com>
2023-04-27 19:56:20 -0400

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+
+        string name = UserName();
+        int number = UserNumber();
+        int square= Numsquare(number);
+
+        Results(name, square);
+    }
+     static void Welcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int Numsquare(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void Results(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
