db3b0651c232ceec7ee4d3b57916086aa49299e3
herendira <here.baez@gmail.com>
2023-05-18 20:57:53 -0400

    Journal Class

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+
+
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    public List<Journal>journallist = new List<Journal>();
+    public string _EntryText;
+
+    public string _Prompt;
+    public string _Date;
+    public string _Writemyday;
+    public string prompts()
+    {
+        var random = new Random();
+        var list = new List<string>{"What was the best part of your day","How do you felt the Spirit this day?","Did you helped someone?","What did you learn?","Which were your feeling during the day?" };
+        int index = random.Next(list.Count);
+        return list[index];
+    }
+    
+    public string timeEntry()
+    {
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+
+        return dateText;
+    }
+
+    public void Write()
+
+    {
+        Console.WriteLine("It's time to write:  ");
+        Journal journal = new Journal();
+        journal._Writemyday = $"Date {_Date}, {_EntryText}";
+
+        journallist.Add(journal);
+    }
+
+    public void Display()
+    {
+        
+        foreach(Journal i in journallist)
+        {
+            Console.WriteLine(i._Writemyday);
+        }
+        
+    }
+        public string ToCsvString()
+    {
+        return $"{_Date},{_Prompt},{_EntryText}";
+    }
+
+    
+}
