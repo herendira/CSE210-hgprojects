b61e6f4f3f60b30a79a61a57fbdda6c5ca3ce9be
herendira <here.baez@gmail.com>
2023-05-26 19:38:16 -0400

    Word changes

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -2,13 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-class Word
+class Word                         //Class Word with all its members variables private
 {   private List<int> _numderandom;
     private string _newwords;
    
     private string [] _word2= new string[]{};
     private string [] _word3= new string[]{};
    
+
+    //Word Constructors
     public Word(string words)
     {
       _newwords=words;
@@ -25,8 +27,8 @@ class Word
 
   
 
-  
-public void scripsplit()
+  //Word methods
+public void ScripSplit()
 {  
   
    Scripture scripture= new Scripture();
@@ -36,7 +38,7 @@ public void scripsplit()
   
 }
 
-public void scriptsplit2()
+public void ScripSplit2()
 { 
    Scripture scripture2= new Scripture();
    string textoscrip2=scripture2.GetScripture2();
@@ -46,7 +48,7 @@ public void scriptsplit2()
 }
 
 
-public void wordrandom()
+public void WordRandom()
 {
     Random generator = new Random();
     for (int i = 0; i < _word2.Length; i++)
@@ -72,7 +74,7 @@ public void wordrandom()
 
 
 }
-public void wordrandom2()
+public void WordRandom2()
 {
     Random generator = new Random();
     for (int i = 0; i < _word3.Length; i++)
@@ -89,7 +91,7 @@ public void wordrandom2()
         _numderandom.RemoveAt(randomIndex); 
         string _newwords = string.Join(" ", _word3); 
         Console.Clear();
-        Reference reference=new Reference();
+        Reference reference=new Reference();  //Calling class Reference
         reference.GetReference();
         Console.WriteLine(_newwords); 
         
