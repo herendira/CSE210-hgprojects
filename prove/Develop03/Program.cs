7a42a0c3f87cdc8b72f92fb4f4abe829d4f11c1e
herendira <here.baez@gmail.com>
2023-05-26 18:33:25 -0400

    Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,32 +3,81 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        //Console.WriteLine("Hello Develop03 World!");
-        fraction f1 = new fraction();
-        //f1.GetDecimalValue();
+   {     
         
-        Console.WriteLine(f1.GetFraction());
-        Console.WriteLine(f1.GetDecimalValue());
-        
-        fraction f2 = new fraction(5);
-        //f2.GetDecimalValue();
-        Console.WriteLine(f2.GetFraction());
-        Console.WriteLine(f2.GetDecimalValue());
-        
-
-        fraction f3 = new fraction(3,4);
-        //f3.GetDecimalValue();
-        Console.WriteLine(f3.GetFraction());
-        Console.WriteLine(f3.GetDecimalValue());
-        
-
-        fraction f4 = new fraction(1,3);
-        Console.WriteLine(f4.GetFraction());
-        Console.WriteLine(f4.GetDecimalValue());
-        fraction f5 = new fraction(54,63);
-         Console.WriteLine(f5.GetFraction());
+        Console.WriteLine(" \n\n\n             S C R I P T U R E    M E M O R I Z E       \n");
+       
+        bool op = true;
+        while (op)
+        {
+            Console.WriteLine("Enter to continue");
+            Console.WriteLine("Write quit to finish\n");
+            string option= Console.ReadLine();
+            if (option=="")
+              {    
+                  //Reference and Scripture instances
+                  Console.WriteLine("Option A");
+                  Reference reference2 = new Reference();
+                  Console.WriteLine(reference2.GetReference("2 Nefi","2","6"));
+                  Scripture scripture = new Scripture();
+           
+                  Console.WriteLine(scripture.GetScripture());
+                  Console.WriteLine("\n\n");
 
 
-    }
+
+                  Console.WriteLine("Option B");
+                  Reference reference3 = new Reference();
+                  Console.WriteLine(reference3.GetReference("Alma","35","4","5"));
+                  Scripture scripture2 = new Scripture();
+                  Console.WriteLine(scripture2.GetScripture2());
+                  Console.WriteLine("\n\n");
+                  Word word = new Word();
+
+                  Console.WriteLine("What Scripture would you like to learn? A or B");
+                  string quiero=Console.ReadLine();
+                  if (quiero=="A")
+                  {  
+                     Console.WriteLine(reference2.GetReference("2 Nefi","2","6"));
+                     Console.WriteLine(scripture.GetScripture());
+                     Console.WriteLine("\nReady??? GO!!! press enter to continue\n");
+                     word.scripsplit();
+                     Console.WriteLine(reference2.GetReference("2 Nefi","2","6"));
+                     word.wordrandom();
+                  }
+                  if (quiero=="B")
+                  { 
+                    Console.WriteLine(reference3.GetReference("Alma","35","4","5"));
+                    Console.WriteLine(scripture2.GetScripture2());
+                    Console.WriteLine("\nReady??? GO!!! press enter to continue\n");
+                    word.scriptsplit2();
+                    Console.WriteLine(reference3.GetReference("Alma","35","4","5"));
+                    word.wordrandom2();
+                }
+              }
+
+                  
+
+              
+               
+            else if (option == "quit")
+                {
+                  Console.WriteLine("Keep going learning the Scriptures");
+                  op=false;
+                }
+            else
+               {
+                Console.WriteLine("Select enter to continue, or quit to finish");
+                continue;
+               }  
+             }  
+            
+
+    
+   
+    
+   } 
+
+  
+ 
 }
---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Reference
+{
+    private string _book;
+    private string _chapter;
+    private string _verse;
+    private string _verse2;
+    
+//constructors
+    public Reference()
+    {
+        _book="Marcos";
+        _chapter="15";
+        _verse="3";
+       
+    } 
+    public Reference(string b,string c,string v1)
+    {
+        _book=b;
+        _chapter=c;
+        _verse=v1;
+       
+    }      
+    public Reference(string b,string c,string v1,string v2)
+    {
+        _book=b;
+        _chapter=c;
+        _verse=v1;
+        _verse2=v2;
+    }   
+    
+
+
+//getters
+public string GetReference()
+{
+    string refe=($"{_book} {_chapter}:{_verse}");
+    return refe;
+}
+
+public string GetReference(string b, string c, string v1)
+{
+    string refe=($"{b} {c}:{v1}");
+    return refe;
+}
+
+public string GetReference(string b, string c, string v1, string v2)
+{
+    string refe=($"{b} {c}:{v1}-{v2}");
+    return refe;
+}
+
+
+public void Hide()
+{
+   
+}
+public void IsHidden()
+{
+
+}
+public void Randomwords()
+{
+
+}
+public void DisplayLines()
+{
+
+}
+
+
+
+
+
+}
+
+
