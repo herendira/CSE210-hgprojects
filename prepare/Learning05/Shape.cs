2d2b009c53e02cc20fe80cd3579e7aecf0765d8a
herendira <here.baez@gmail.com>
2023-06-13 21:23:23 -0400

    Shape Class

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+class Shape                   
+{
+    private string _color;
+    
+    
+    
+    public Shape (string color)
+    {
+       _color = color;
+      
+    }
+    public string GetColor()
+    {
+         return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+   
+    
+    
+    public virtual double GetArea()
+    {
+        return -1;
+    }
+
+}
+    
