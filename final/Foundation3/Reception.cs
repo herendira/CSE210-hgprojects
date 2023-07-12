cff1821d2b6decc5616a0784500a4b9495e231c5
herendira <here.baez@gmail.com>
2023-07-12 17:48:27 -0400

    Reception Class

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public class Reception:Event
+{
+
+    private string _registration;
+    
+   
+    //constructors
+    public Reception(string type, string title, string description, string date, string time, Address addressevent, string registration):base(type, title, description, date, time, addressevent)
+    {
+        _registration = registration;
+    }
+
+    public void SetRegistration(string registration)
+    {
+        _registration = registration;
+    } 
+
+    public string GetRegistration()
+    {
+        return _registration;
+    }
+
+    //Methods
+    public override string GetEvent()
+    {
+        return $"{base.GetEvent()}\nReception: {_registration}";
+    }
+}
