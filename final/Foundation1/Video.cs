9e7a39869ec8b7109c7a5e3143ee306e1201a6d6
herendira <here.baez@gmail.com>
2023-07-16 18:56:48 -0400

    Update

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -8,6 +8,7 @@ public  class Video
     public string _title;
     public string _author;
     public float _durationinsec;
+    public List<Comment> _comments;
    
     //constructors
     public Video(string title, string author, float durationinsec)
@@ -15,6 +16,7 @@ public  class Video
         _title = title;
         _author = author;
         _durationinsec = durationinsec;
+        _comments = new List<Comment>();
     }
 
    
@@ -31,6 +33,20 @@ public  class Video
     {
         return _durationinsec;
     }
-    
+    public int GetComNumber()
+    {
+        return _comments.Count;
+    }
+    public void AddingComments(Comment comment)
+    {
+         _comments.Add(comment);
+    }
+    public void DisplayComments()
+    {
+       foreach(Comment comment in _comments)
+       {
+            Console.WriteLine($"{comment._commenter}, {comment._comment}");
+       }
+    }  
     
 }
